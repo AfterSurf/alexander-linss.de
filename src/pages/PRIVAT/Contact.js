3ae30bf2d21f44f51732de9ae24345524e2a71b7
BaLin <alexanderlinss@web.de>
2023-04-15 18:22:59 +0200

    added profile pic

--- src/pages/PRIVAT/Contact.js
+++ src/pages/PRIVAT/Contact.js
@@ -2,17 +2,13 @@ import React from "react";
 import { useNav } from "../../customHooks/useNav";
 
 import "./../Page.css";
+import image from "../../pictures/alex_bw.jpg";
 
 const Contact = () => {
   const contactRef = useNav("ContactIT");
-  console.log("contact");
-
   return (
     <section ref={contactRef} id="contactContainerIt">
-      <img
-        src="https://source.unsplash.com/random/600x600/?nature,water"
-        alt="unsplash-img"
-      />
+      <img src={image} alt="unsplash-img" />
       <div>
         <h3>CONTACT</h3>
         <p>This is the contact section</p>
