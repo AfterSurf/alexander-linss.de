a386f6e06693b0d830f8320fb5cc6b144deff8a1
Privat <privat@Laptopvlexander.fritz.box>
2023-07-14 19:54:22 +0200

    added tiny gridlayout

--- src/pages/PRIVAT/Contact/Contact.js
+++ src/pages/PRIVAT/Contact/Contact.js
@@ -9,10 +9,10 @@ const Contact = () => {
   const contactRef = useNav("Contact");
   return (
     <section ref={contactRef} id="contactContainerPrivat">
-      <img
+      {/* <img
         src="https://source.unsplash.com/random/600x600/?nature,water"
         alt="unsplash-img"
-      />
+      /> */}
       <div>
         <h3>CONTACT</h3>
         <p>This is the contact section</p>
--- src/pages/PRIVAT/Contact/Form.js
+++ src/pages/PRIVAT/Contact/Form.js
@@ -1,5 +1,5 @@
 
-
+import "./Form.css"
 const Form = () => {
 
     const onSubmit = () => {
@@ -8,31 +8,35 @@ const Form = () => {
 
     return (
         <div>
-            <div className="container mt-5">
-                <h2 className="mb-3">React Contact Form Component Example</h2>
+            <div className="container ">
                 <form onSubmit={onSubmit}>
-                    <div className="mb-3">
+                <h2 className="">React Contact Form Component Example</h2>
+                    <div className="">
                         <label className="form-label" htmlFor="name">
                             Name
                         </label>
                         <input className="form-control" type="text" id="name" required />
                     </div>
-                    <div className="mb-3">
+                    <div className="">
                         <label className="form-label" htmlFor="email">
                             Email
                         </label>
                         <input className="form-control" type="email" id="email" required />
                     </div>
-                    <div className="mb-3">
+                    <div className="">
                         <label className="form-label" htmlFor="message">
                             Message
                         </label>
                         <textarea className="form-control" id="message" required />
                     </div>
                     {/* <button className="btn btn-danger" type="submit"> */}
+                    
+                    <div className="">
                     <button className="btn btn-danger" type="button" onClick={onSubmit}>
                         {/* {formStatus} */}BUTTON
                     </button>
+                    </div>
+                
                 </form>
             </div>
         </div>
