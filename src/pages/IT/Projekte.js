1077f1acf58a671923b8c62823962d447a26b0cd
Alexander Linß <alexanderlinss@MacBook-Pro-von-Alexander.fritz.box>
2024-09-06 19:50:03 +0200

    imageSpielereien

--- src/pages/IT/Projekte.js
+++ src/pages/IT/Projekte.js
@@ -1,16 +1,25 @@
-import React from "react";
+// import React from "react";
 import { useNav } from "../../customHooks/useNav";
 
 import "./../Page.css";
+import Image from 'next/image';
+
+
 
 const Projekte = () => {
   const contactRef = useNav("Projects");
+
+
+
+
+
+
+
   return (
+    <>
     <section ref={contactRef} id="contactItContainer">
-      <img
-        src="https://source.unsplash.com/random/600x600/?nature,water"
-        alt="unsplash-img"
-      />
+      {/* Hier checken wir man das Image einbindet */}
+    <Image src={'swv_logo.png'} width={150} height={150} />
       <div>
         <h3>Projekt(e)</h3>
         <p>Aktuell möchte ich vorstellen wie meine Homepage aufgebaut ist: </p>
@@ -30,6 +39,7 @@ const Projekte = () => {
         </ul>
       </div>
     </section>
+    </>
   );
 };
 
