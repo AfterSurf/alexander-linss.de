113244eb39d9037827e0d78a4aa2425ae47a4de1
Alexander Linß <alexanderlinss@MacBook-Pro-von-Alexander.local>
2024-09-29 01:04:59 +0200

    startd

--- src/components/IT/Home.js
+++ src/components/IT/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNav } from "../../customHooks/useNav";
-import styles from "./itStyles.module.css"
+import styles from "./itStyles.module.css";
 
 import Points from "./Points/Points";
 import Why from "./Points/Why";
@@ -10,52 +10,87 @@ const Home = () => {
   const homeRef = useNav("Home");
   return (
     <section ref={homeRef} id="homeItContainer">
-      <div>
+      <div className={styles.PointsContainer}>
         {textComponent}
-        <div className={styles.PointsContainer}>
-          <Points title="Warum diese Seite" alternativTitle="Darum diese Seite" Component={<Why/>}/>
-          <Points title="Wo bin ich zu Hause" alternativTitle="Hier bin ich zu Hause" Component={<FavTech/>}/>
-        </div>
-        {profileComponent}
+        
+          {/* <div className={styles.PointsContainer}> */}
+          <Points
+            className={styles.gridElement2}
+            title="Warum diese Seite"
+            alternativTitle="Darum diese Seite"
+            Component={<Why />}
+          />
+          <Points
+
+            className={styles.gridElement3}
+            title="Wo bin ich zu Hause"
+            alternativTitle="Hier bin ich zu Hause"
+            Component={<FavTech />}
+          />
+        
+        {profileComponent4}
+        {profileComponent5}
+        {profileComponent6}
       </div>
     </section>
   );
 };
 
-const text = `Das ist der technischen Teil der Website.`;
+var text = `Das ist der technischen Teil der Website.`;
+text = "AAA";
 
-const textComponent = (
-  <>
-    <pre className={styles.frontText}>
-      <h3>{text}</h3>
-    </pre>
-  </>
-);
+function getTextComponent(text, className) {
+return (  <>
+  <pre className={styles[className]}>
+    <h3>{text}</h3>
+  </pre>
+</>)
+}
 
-const profileComponent = (
+
+const textComponent = getTextComponent("ADSDSA","gridElement1")
+// const profileComponent1 = getTextComponent("gridElement4","gridElement4")
+// const profileComponent2 =  getTextComponent("gridElement5","gridElement5")
+const profileComponent6 =  getTextComponent("gridElement6","gridElement6")
+
+
+const profileComponent4 = (
   <>
-    <div>
+    <div className={styles["gridElement4"]}>
       <pre>
-      <h3>Profile:</h3>
-      <div className={styles.socialmedia}>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/alexander-lin%C3%9F/"
-        >
-          Linked:in
-        </a>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/AfterSurf/"
-        >
-          Git:Hub
-        </a>
-      </div>
+        {/* <h3>Profile:</h3> */}
+        <div className={styles.socialmedia}>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/alexander-lin%C3%9F/"
+          >
+            Linked:in
+          </a>
+        </div>
       </pre>
     </div>
   </>
 );
+
+const profileComponent5 = (
+  <>
+    <div className={styles["gridElement5"]}>
+      <pre>
+        {/* <h3>Profile:</h3> */}
+        <div className={styles.socialmedia}>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/AfterSurf/"
+          >
+            Git:Hub
+          </a>
+        </div>
+      </pre>
+    </div>
+  </>
+);
+
 
 export default Home;
